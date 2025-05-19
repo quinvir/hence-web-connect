af0ae883eaf6ea6895627672c1d34d257a5ff805
Yuna <ingyuna52@gmail.com>
2025-05-19 15:55:53 +0900

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+
+function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+    </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body {
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+      sans-serif;
+    background-color: #fff;
+    color: #333;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font: inherit;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  input, textarea {
+    font: inherit;
+    border: none;
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
